67d38de65a29f72a1edd466ce4ee6a4f3400042b
Ashfana Ashfu <shaikhashfana9@gmail.com>
2021-11-13 19:54:37 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_N_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("Display n terms of natural number and their sum:\n");
+            Console.Write("----------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input Value of terms : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nThe first {0} natural number are :\n", n);
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} ", i);
+              
+            }
+            
+        }
+    }
+}
